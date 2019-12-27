82faeab2b9d83e47388b22fcf55e2d85a7cc1472
ariabinin <ariabinin@varteq.com>
2019-12-27 18:57:08 +0200

    Add JSON back reference annotation to avoid serialization issues

--- src/main/java/waterfall/flowfall/model/Board.java
+++ src/main/java/waterfall/flowfall/model/Board.java
@@ -24,6 +24,7 @@ public class Board {
     @JoinColumn(name="user_id")
     private User user;
 
+    @JsonBackReference
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name="board_collaborator",
