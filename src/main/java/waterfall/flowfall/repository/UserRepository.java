94576928568cdfc7ec50d62629b8227e6a519629
thewaterfall <floodlight98@gmail.com>
2019-12-29 11:53:18 +0200

    Add missed import

--- src/main/java/waterfall/flowfall/repository/UserRepository.java
+++ src/main/java/waterfall/flowfall/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package waterfall.flowfall.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import waterfall.flowfall.model.User;
