ac821cebf4f79bf06afcfc6b29dd38321a694124
thewaterfall <floodlight98@gmail.com>
2020-02-16 19:57:58 +0200

    Refactor security context utils

--- src/main/java/waterfall/flowfall/util/SecurityContextUtils.java
+++ src/main/java/waterfall/flowfall/util/SecurityContextUtils.java
@@ -3,11 +3,13 @@ package waterfall.flowfall.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import waterfall.flowfall.model.User;
 import waterfall.flowfall.service.UserService;
 
 import javax.annotation.PostConstruct;
 
+@Component
 public class SecurityContextUtils {
 
     private static UserService userService;
@@ -21,7 +23,7 @@ public class SecurityContextUtils {
     }
 
     public static User getAuthenticatedUser() {
-        Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return userService.findByEmail(auth.getName()).get();
     }
 }
