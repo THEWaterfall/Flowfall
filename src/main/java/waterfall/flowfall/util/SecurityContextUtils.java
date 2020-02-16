fdc7d2741ce1ad053cdbec6cb5c4bafdee292985
thewaterfall <floodlight98@gmail.com>
2020-02-16 19:06:23 +0200

    Add security context utils

---
+++ src/main/java/waterfall/flowfall/util/SecurityContextUtils.java
@@ -0,0 +1,27 @@
+package waterfall.flowfall.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import waterfall.flowfall.model.User;
+import waterfall.flowfall.service.UserService;
+
+import javax.annotation.PostConstruct;
+
+public class SecurityContextUtils {
+
+    private static UserService userService;
+
+    @Autowired
+    private UserService autowiredUserService;
+
+    @PostConstruct
+    private void init() {
+        userService = this.autowiredUserService;
+    }
+
+    public static User getAuthenticatedUser() {
+        Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
+        return userService.findByEmail(auth.getName()).get();
+    }
+}
