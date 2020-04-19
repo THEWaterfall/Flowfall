eefbbbe1b89b4afb8e0271440bb39eb7bfbff7a1
thewaterfall <floodlight98@gmail.com>
2020-04-19 17:50:40 +0300

    Refactor OAuth2 facade

--- src/main/java/waterfall/flowfall/security/oauth2/OAuth2Facade.java
+++ src/main/java/waterfall/flowfall/security/oauth2/OAuth2Facade.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import waterfall.flowfall.model.GlobalRole;
 import waterfall.flowfall.model.User;
+import waterfall.flowfall.model.UserProfile;
+import waterfall.flowfall.model.enums.UserGlobalRole;
 import waterfall.flowfall.security.AuthFacade;
+import waterfall.flowfall.security.AuthProvider;
 import waterfall.flowfall.security.jwt.JwtResponse;
 import waterfall.flowfall.security.oauth2.userinfo.OAuth2UserInfo;
 import waterfall.flowfall.service.UserService;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 
@@ -47,7 +52,18 @@ public class OAuth2Facade {
         if (optionalUser.isPresent()) {
             return authFacade.authenticate(optionalUser.get());
         } else {
-            return authFacade.register(provider, userInfo);
+            return register(provider, userInfo);
         }
+    }
+
+    public JwtResponse register(String provider, OAuth2UserInfo userInfo) {
+        User user = new User();
+        user.setProvider(AuthProvider.valueOf(provider.toUpperCase()));
+        user.setEmail(userInfo.getEmail());
+        user.setProfile(new UserProfile(userInfo.getName()));
+        user.setGlobalRoles(Arrays.asList(new GlobalRole(UserGlobalRole.USER)));
+        user.setVerified(true);
+
+        return authFacade.authenticate(userService.save(user));
     }
 }
