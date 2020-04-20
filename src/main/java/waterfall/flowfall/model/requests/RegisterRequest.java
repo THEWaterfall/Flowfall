db3aaa348db5c31f54dfb03c554be6f5b3d8579e
thewaterfall <floodlight98@gmail.com>
2020-04-20 17:43:47 +0300

    Add register request class

---
+++ src/main/java/waterfall/flowfall/model/requests/RegisterRequest.java
@@ -0,0 +1,44 @@
+package waterfall.flowfall.model.requests;
+
+import waterfall.flowfall.validation.annotations.VacantEmail;
+
+public class RegisterRequest {
+    private String fullname;
+
+    @VacantEmail
+    private String email;
+    private String password;
+    private String redirectUri;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+}
